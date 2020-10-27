7f1ee31464f2f666459c642273b651f3fab202ee
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-10-27 10:21:59 +0530

    Add Display owner view. Closes #53

--- pet-clinic-web/src/test/java/com/harish/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/harish/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -16,7 +16,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -67,6 +68,16 @@ class OwnerControllerTest {
     void findOwners() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/find")).andExpect(status().isOk())
                 .andExpect(view().name("notImplemented"));
-       verifyNoInteractions(ownerService);
+        verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/123"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
